c382480dff460edd87e8efd7e442b594c18fa9c8
Abhishek Khanna <Abhishekkhanna1995@gmail.com>
2024-08-17 18:20:38 +0530

    feat(gsu-backend): added task models

---
+++ src/models/task.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface ITask extends Document {
+  title: string;
+  description: string;
+  status: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const TaskSchema: Schema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    status: {
+      type: String,
+      required: true,
+      enum: ["todo", "inprogress", "complete", "backlog", "blocked"],
+      default: "todo",
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model<ITask>("Task", TaskSchema);
